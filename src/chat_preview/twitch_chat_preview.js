caf662f65d20e38c3832c5fb96481cda301b61c6
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-08-16 02:37:06 +0300

    text to speach initial display & animations

--- src/chat_preview/twitch_chat_preview.js
+++ src/chat_preview/twitch_chat_preview.js
@@ -4,3 +4,17 @@ let twitchStreamer = "xqc"
 
 let messageUpdater = new MessageUpdater(twitchStreamer);
 
+
+// tts content 
+var fieldset = document.getElementById("tts-fieldset");
+var content = document.getElementById("tts-content");
+
+document.getElementById("tts-arrow").onclick = function () {
+  if (fieldset.classList.contains("tts-closed")) {
+    content.style.maxHeight = content.scrollHeight + "px";
+    fieldset.classList.remove("tts-closed");
+  } else {
+    content.style.maxHeight = "0px";
+    fieldset.classList.add("tts-closed");
+  }
+}
