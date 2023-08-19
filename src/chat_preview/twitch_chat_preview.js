8ad51724a88c7dbe759312f323fb4541dcf9361f
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-08-19 21:52:23 +0300

    from twitch_caht_preview.js removed MessageSpeakOptions import

--- src/chat_preview/twitch_chat_preview.js
+++ src/chat_preview/twitch_chat_preview.js
@@ -1,6 +1,5 @@
 import { MessageUpdater } from "./message_updater/messageUpdater.js";
 import { TextToSpeach } from "./text_to_speach/textToSpeach.js";
-import { MessageSpeakOptions } from "./text_to_speach/messageSpeakOptions.js";
 
 let twitchStreamer = "xqc"
 
