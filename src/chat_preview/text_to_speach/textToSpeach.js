37030ab8208e04438ff9d63c6585da9f9c93f2b7
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-08-19 19:25:41 +0300

    linked HTML with javascript for TTS

--- src/chat_preview/text_to_speach/textToSpeach.js
+++ src/chat_preview/text_to_speach/textToSpeach.js
@@ -2,23 +2,38 @@ import { MessageDTO } from "../message_updater/messageDTO.js";
 import { MessageSpeakOptions } from "./messageSpeakOptions.js";
 
 export class TextToSpeach {
+    
     #isAvailable
+    #isActive = false;
+    #defaultSpeakOptions = new MessageSpeakOptions();
 
     constructor(){
         this.#isAvailable = window.speechSynthesis != null;
+
+        this.#setTtsDefaults(this.#isActive, this.#defaultSpeakOptions);
+        this.#setTtsOptionEvents();
     }
 
     isAvailable = () => {
         return this.#isAvailable;
     }
 
+    isActive = () => {
+        return this.#isActive;
+    }
+
     reset = () =>{
         window.speechSynthesis.cancel();
     }
 
-    speak = (messageDTO, messageSpeakOptions) => {
+    speak = (messageDTO, messageSpeakOptions = null) => {
+        messageSpeakOptions ??= this.#defaultSpeakOptions;
+
         if (!this.#isAvailable){
-            return
+            return;
+        }
+        if (!this.#isActive){
+            return;
         }
         if (!(messageDTO instanceof MessageDTO)) {
             return;
@@ -34,6 +49,8 @@ export class TextToSpeach {
         if (!canSpeak) {
             return;
         }
+
+
         let message = new SpeechSynthesisUtterance(this.#messageComposer(messageDTO, messageSpeakOptions));
         message.rate = messageSpeakOptions.speed;
         window.speechSynthesis.speak(message);
@@ -46,5 +63,55 @@ export class TextToSpeach {
         }
         message += messageDTO.message;
         return message;
+    }
+
+
+
+    #setTtsDefaults = (isActive, currentSpeakOptions) => {
+        // set the object's values
+        document.getElementById("tts-arrow").checked = isActive;
+        document.getElementById("tts-all-toggle").checked = currentSpeakOptions.all;
+        document.getElementById("tts-sub-toggle").checked = currentSpeakOptions.subscribers;
+        document.getElementById("tts-mod-toggle").checked = currentSpeakOptions.mods;
+        document.getElementById("tts-turbo-toggle").checked = currentSpeakOptions.turbo;
+        document.getElementById("tts-name-toggle").checked = currentSpeakOptions.includeDisplayName;
+        document.getElementById("tts-speed-toggle").value = currentSpeakOptions.speed;
+        document.getElementById("tts-length-toggle").value = currentSpeakOptions.maxMessageLength;
+    }
+
+    #setTtsOptionEvents = () => {
+        // events to change the tts state
+        document.getElementById("tts-arrow").addEventListener("input", (ev) => {
+            this.#isActive = document.getElementById("tts-arrow").checked;
+            this.reset();
+        });
+        document.getElementById("tts-all-toggle").addEventListener("input", (ev) => {
+            this.#defaultSpeakOptions.all = document.getElementById("tts-all-toggle").checked;
+            this.reset();
+        });
+        document.getElementById("tts-sub-toggle").addEventListener("input", (ev) => {
+            this.#defaultSpeakOptions.subscribers = document.getElementById("tts-sub-toggle").checked;
+            this.reset();
+        });
+        document.getElementById("tts-mod-toggle").addEventListener("input", (ev) => {
+            this.#defaultSpeakOptions.mods = document.getElementById("tts-mod-toggle").checked;
+            this.reset();
+        });
+        document.getElementById("tts-turbo-toggle").addEventListener("input", (ev) => {
+            this.#defaultSpeakOptions.turbo = document.getElementById("tts-turbo-toggle").checked;
+            this.reset();
+        });
+        document.getElementById("tts-name-toggle").addEventListener("input", (ev) => {
+            this.#defaultSpeakOptions.includeDisplayName = document.getElementById("tts-name-toggle").checked;
+            this.reset();
+        });
+        document.getElementById("tts-speed-toggle").addEventListener("input", (ev) => {
+            this.#defaultSpeakOptions.speed = document.getElementById("tts-speed-toggle").value;
+            this.reset();
+        });
+        document.getElementById("tts-length-toggle").addEventListener("input", (ev) => {
+            this.#defaultSpeakOptions.maxMessageLength = document.getElementById("tts-length-toggle").value;
+            this.reset();
+        });
     }
 }
