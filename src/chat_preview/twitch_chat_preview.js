0fd166af3aac934e3047ad2cb1ecd5f987fe7186
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-08-19 20:33:22 +0300

    created tts object

--- src/chat_preview/twitch_chat_preview.js
+++ src/chat_preview/twitch_chat_preview.js
@@ -1,8 +1,12 @@
 import { MessageUpdater } from "./message_updater/messageUpdater.js";
+import { TextToSpeach } from "./text_to_speach/textToSpeach.js";
+import { MessageSpeakOptions } from "./text_to_speach/messageSpeakOptions.js";
 
 let twitchStreamer = "xqc"
 
 let messageUpdater = new MessageUpdater(twitchStreamer);
+let tts = new TextToSpeach();
+messageUpdater.onMessageReceived.add(null, (message) => tts.speak(message));
 
 
 // tts content 
