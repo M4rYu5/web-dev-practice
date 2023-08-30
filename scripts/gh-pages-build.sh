5732e0754ad60a54d6c3eb98b1ab2e11c4e0b75e
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-08-30 18:23:45 +0300

    Added GitHub Actions for build and deploy

---
+++ scripts/gh-pages-build.sh
@@ -0,0 +1,5 @@
+# temp-build is the root folder of website
+rm -r "temp-build"
+mkdir "temp-build"
+cp -r "../src/chat_preview" "temp-build/chat_preview"
+cp -r "../src/pagination_practice" "temp-build/pagination_practice"
