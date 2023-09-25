b178638c129b821078cca6baf9e7bd528d2b5715
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-09-25 17:40:26 +0300

    products states in main page

--- src/store_front_react/app/page.tsx
+++ src/store_front_react/app/page.tsx
@@ -1,10 +1,19 @@
-"use client"
+"use client";
 
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
+import { getProducts } from "./../data/repository";
+import { ProductFilter } from "@/data/ProductFilter";
+import { ProductPreview } from "@/data/ProductPreview";
 
 const Home = () => {
-  return (
-    <span>Home</span>
-  )
-}
+  let [filter, setFilter] = useState(new ProductFilter(0, 10));
+  let [products, setProducts]: [ProductPreview[] | null, any] = useState(null);
+
+  useEffect(() => {
+    getProducts(filter).then((x) => setProducts(x));
+  }, [filter]);
+
+  return <div>{JSON.stringify(products)}</div>;
+};
 
 export default Home;
