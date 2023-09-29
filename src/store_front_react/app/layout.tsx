894376bf1849794e4eac9303289a04eb0158789f
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-09-29 19:20:11 +0300

    background wierdnes hot fix

--- src/store_front_react/app/layout.tsx
+++ src/store_front_react/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" style={{ minHeight: "100%" }}>
-      <body className={inter.className}>
+    <html lang="en" style={{ minHeight: "100%"}} className="bg-black bg-gradient-to-tr from-black">
+      <body className={inter.className + " bg-transparent bg-gradient-to-tr from-transparent"} style={{ minHeight: "100%"}}>
         <BasketProvider>
           <SiteMenu />
           <div className="container mx-auto mt-5 sm:px-12 px-5">{children}</div>
