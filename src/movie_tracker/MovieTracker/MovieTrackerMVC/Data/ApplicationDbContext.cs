8012bfcfec1416891da1680156e397f538dd56b3
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-11-12 12:59:35 +0200

    renamed IdentityDbContext to ApplicationDbContext
    also moved from areas/identity/data to data namespace

--- src/movie_tracker/MovieTracker/MovieTrackerMVC/Data/ApplicationDbContext.cs
+++ src/movie_tracker/MovieTracker/MovieTrackerMVC/Data/ApplicationDbContext.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MovieTrackerMVC.Models;
 
-namespace MovieTrackerMVC.Data
+namespace MovieTrackerMVC.Data;
+
+public class ApplicationDbContext : IdentityDbContext<User>
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
-        {
-        }
+    }
 
-
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
     }
 }
