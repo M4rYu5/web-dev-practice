8e3c5aeeeb7bb223d5787ec2c33e7b5d4f6965fc
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-11-16 01:45:47 +0200

    added next option in UserMediaStatus

---
+++ src/movie_tracker/MovieTracker/MovieTrackerMVC/Migrations/20231115234302_AddedNextOptionInUserMediaStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieTrackerMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNextOptionInUserMediaStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserMediaStatus",
+                columns: new[] { "Id", "Color", "Name" },
+                values: new object[] { -85L, -8388480, "Next" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserMediaStatus",
+                keyColumn: "Id",
+                keyValue: -85L);
+        }
+    }
+}
