eaade4b376eb44db8e3f1ab25e03a05ef4a63454
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-11-20 04:22:08 +0200

    added storage api with cover endpoints

---
+++ src/movie_tracker/MovieTracker/StorageAPI/Endpoints/CoverHandlers.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+
+namespace StorageAPI.Endpoints
+{
+    public static class CoverHandlers
+    {
+
+        public const long MAX_UPLOAD_IMAGE_SIZE = 200 * 1024; // 200KB
+
+
+        internal static IResult DeleteCover(HttpContext context, string id)
+        {
+            if (!ValidId(id, out var badResult))
+                return badResult ?? throw new UnreachableException();
+
+            try
+            {
+                File.Delete("/cover/" + id);
+            }
+            catch (Exception) { }
+
+            return Results.Ok();
+        }
+
+        internal static async Task<IResult> PutCover(HttpContext context, ApiKey apiKey, string id, IFormFile file)
+        {
+            if (ValidId(id, out var badResult))
+                return badResult ?? throw new UnreachableException();
+
+            if (apiKey == null)
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+
+            var providedKey = context.Request.Headers["api-key"];
+            if (string.IsNullOrEmpty(providedKey) || providedKey != apiKey.Key)
+                return Results.Unauthorized();
+
+            var fileSizeInKb = file.Length;
+            if (fileSizeInKb > MAX_UPLOAD_IMAGE_SIZE)
+                return Results.BadRequest($"File should be under {MAX_UPLOAD_IMAGE_SIZE}KB.");
+
+            var fileExtension = Path.GetExtension(file.FileName)?.ToLower();
+            if (fileExtension != ".png")
+                return Results.BadRequest("Expected .png file.");
+
+            var mimeType = file.ContentType.ToLower();
+            if (mimeType != "image/x-png" &&
+                mimeType != "image/png")
+                return Results.BadRequest("Wrong mime type; Expected image/x-png or image/png.");
+
+
+            using var toFileStream = new FileStream("/cover/id" + fileExtension, FileMode.Create);
+            await file.CopyToAsync(toFileStream);
+
+            return Results.Ok();
+        }
+
+
+        /// <summary>
+        /// Returns whether or not the given id is valid. Must have only alphanumeric characters or -
+        /// </summary>
+        /// <param name="id"> the id to check </param>
+        /// <param name="badResult"> Has a value only if the id is not valid </param>
+        private static bool ValidId(string id, out IResult? badResult)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                badResult = Results.BadRequest("id parameter must have a value");
+                return false;
+            }
+            if (id.Any(c => !char.IsLetterOrDigit(c) && c != '-'))
+            {
+                badResult = Results.BadRequest("id parameter must contain only alphanumeric characters or -");
+                return false;
+            }
+
+            badResult = null;
+            return true;
+        }
+    }
+}
---
+++ src/movie_tracker/MovieTracker/StorageAPI/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+namespace StorageAPI
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateSlimBuilder(args);
+
+            ApiKey key = new() { Key = builder.Configuration["Api:Key"] };
+            builder.Services.AddSingleton(key);
+
+            var app = builder.Build();
+
+            app.MapPut("/cover/{id}", Endpoints.CoverHandlers.PutCover);
+            app.MapDelete("/cover/{id}", Endpoints.CoverHandlers.DeleteCover);
+            app.UseStaticFiles("/cover");
+
+            app.Run();
+        }
+
+
+
+    }
+
+
+    public class ApiKey
+    {
+        public string? Key { get; set; }
+    }
+}
+
