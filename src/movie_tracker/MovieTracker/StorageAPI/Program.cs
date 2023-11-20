43c9312214bf087aef5856132cfc47a1c74d92bf
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-11-20 23:54:40 +0200

    added output cache

--- src/movie_tracker/MovieTracker/StorageAPI/Program.cs
+++ src/movie_tracker/MovieTracker/StorageAPI/Program.cs
@@ -18,12 +18,20 @@ namespace StorageAPI
             ApiKey key = new() { Key = builder.Configuration["Api:Key"] };
             builder.Services.AddSingleton(key);
 
+            builder.Services.AddOutputCache(options =>
+            {
+                options.AddBasePolicy(builder => builder.Expire(TimeSpan.FromMinutes(5)));
+            });
+
+
             var app = builder.Build();
 
             app.UseMiddleware<ApiKeyAuthorizationMiddleware>();
 
             app.MapPut("/cover/{id}", Endpoints.CoverHandlers.PutCover).DisableAntiforgery();
             app.MapDelete("/cover/{id}", Endpoints.CoverHandlers.DeleteCover);
+
+            app.UseOutputCache();
             app.UseStaticFiles("/cover");
 
             app.Run();
