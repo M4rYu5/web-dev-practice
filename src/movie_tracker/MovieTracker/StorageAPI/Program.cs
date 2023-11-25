e0da7979c26e385d80261b621f55529a2fce8e99
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-11-25 12:35:48 +0200

    ApiKey is now in Configuration

--- src/movie_tracker/MovieTracker/StorageAPI/Program.cs
+++ src/movie_tracker/MovieTracker/StorageAPI/Program.cs
@@ -15,14 +15,11 @@ namespace StorageAPI
         {
             var builder = WebApplication.CreateSlimBuilder(args);
 
-            ApiKey key = new() { Key = builder.Configuration["Api:Key"] };
-            builder.Services.AddSingleton(key);
-
+            builder.Services.Configure<ApiKey>(option => option.Key = builder.Configuration["Api:Key"]);
             builder.Services.AddOutputCache(options =>
             {
                 options.AddBasePolicy(builder => builder.Expire(TimeSpan.FromMinutes(5)));
             });
-
 
             var app = builder.Build();
 
