5772acdda237c0de9e32256a7166cbebbda11e28
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-12-12 07:47:16 +0200

    storage api, cover endpoint is now taking a full and resized_150 file

--- src/movie_tracker/MovieTracker/StorageAPI/Endpoints/CoverHandlers.cs
+++ src/movie_tracker/MovieTracker/StorageAPI/Endpoints/CoverHandlers.cs
@@ -1,12 +1,18 @@
 ﻿using StorageAPI.AppConfig.Attributes;
+using StorageAPI.Extensions;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 
 namespace StorageAPI.Endpoints
 {
-    public class CoverHandlers
+    public sealed class CoverHandlers
     {
 
-        public const long MAX_UPLOAD_IMAGE_SIZE = 200 * 1024; // 200KB
+        public const long MAX_UPLOAD_IMAGE_SIZE_FULL = 2 * 1024 * 1024; // 2MB
+        public const long MAX_UPLOAD_IMAGE_SIZE_150 = 200 * 1024; // 200KB
+        private const string COVER_EXTENSION = ".png";
+
+        private static readonly string[] ALLOWED_MIME = ["image/x-png", "image/png"];
 
 
         internal static IResult DeleteCover(HttpContext context, string id)
@@ -24,28 +30,32 @@ namespace StorageAPI.Endpoints
         }
 
         [ApiKeyAuthorization]
-        internal static async Task<IResult> PutCover(string id, IFormFile file, ILogger<CoverHandlers> logger)
+        internal static async Task<IResult> PutCover(string id, IFormFile full, IFormFile resized_150, ILogger<CoverHandlers> logger)
         {
             if (!ValidId(id, out var badResult))
                 return badResult ?? throw new UnreachableException();
 
-            var fileSizeInKb = file.Length;
-            if (fileSizeInKb > MAX_UPLOAD_IMAGE_SIZE)
-                return Results.BadRequest($"File should be under {MAX_UPLOAD_IMAGE_SIZE}KB.");
+            if (full.SizeGreaterThan(MAX_UPLOAD_IMAGE_SIZE_FULL))
+                return Results.BadRequest($"'full' cover file should be under {MAX_UPLOAD_IMAGE_SIZE_FULL}KB.");
+            if (resized_150.SizeGreaterThan(MAX_UPLOAD_IMAGE_SIZE_150))
+                return Results.BadRequest($"'resized_150' cover file should be under {MAX_UPLOAD_IMAGE_SIZE_150}KB.");
 
-            var fileExtension = Path.GetExtension(file.FileName)?.ToLower();
-            if (fileExtension != ".png")
-                return Results.BadRequest("Expected .png file.");
+            if (!full.EndsWithExtension(COVER_EXTENSION))
+                return Results.BadRequest($"Expected 'full' file name to end with {COVER_EXTENSION}.");
+            if (!resized_150.EndsWithExtension(COVER_EXTENSION))
+                return Results.BadRequest($"Expected 'resized_150' file name to end with {COVER_EXTENSION}.");
 
-            var mimeType = file.ContentType.ToLower();
-            if (mimeType != "image/x-png" &&
-                mimeType != "image/png")
-                return Results.BadRequest("Wrong mime type; Expected image/x-png or image/png.");
+            if (!full.HasAnyMimeType(ALLOWED_MIME))
+                return Results.BadRequest("full: Wrong mime type; Expected image/x-png or image/png.");
+            if (!resized_150.HasAnyMimeType(ALLOWED_MIME))
+                return Results.BadRequest("resized_150: Wrong mime type; Expected image/x-png or image/png.");
 
             try
             {
-                using var toFileStream = new FileStream($"/storage/cover/{id}" + fileExtension, FileMode.Create);
-                await file.CopyToAsync(toFileStream);
+                using var fullFileStream = new FileStream($"/storage/cover/{id}-full{COVER_EXTENSION}", FileMode.Create);
+                using var resized_150_FileStream = new FileStream($"/storage/cover/{id}-150{COVER_EXTENSION}", FileMode.Create);
+                await full.CopyToAsync(fullFileStream);
+                await resized_150.CopyToAsync(resized_150_FileStream);
             }
             catch (Exception e)
             {
