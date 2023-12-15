1ae394c890321cf1fe3f7a1d58ee5ea07692f2ff
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2023-12-15 15:30:00 +0200

    added asset prefix
    closes #66

--- src/store_front_react/next.config.js
+++ src/store_front_react/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/web-dev-practice/store_front_react/' : '',
 }
 
 module.exports = nextConfig
