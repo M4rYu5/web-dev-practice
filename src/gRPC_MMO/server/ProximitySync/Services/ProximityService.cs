4f0c0acf1aa2c62ee7f4cf9caf5c1d83415e9358
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2024-04-30 19:58:38 +0300

    server: added guards and 'small dev testing' on UpdatePlayers

--- src/gRPC_MMO/server/ProximitySync/Services/ProximityService.cs
+++ src/gRPC_MMO/server/ProximitySync/Services/ProximityService.cs
@@ -19,15 +19,62 @@ public class ProximityService : ProximityUpdater.ProximityUpdaterBase
     }
 
 
-
     public override async Task UpdatePlayers(Empty request, IServerStreamWriter<Players> responseStream, ServerCallContext context)
     {
-        while (true)
+        while (!context.CancellationToken.IsCancellationRequested)
         {
-            await Task.Delay(500);
-            var players = new Players();
-            players.Players_.AddRange(_pm.GetPlayers());
-            await responseStream.WriteAsync(players);
+            try
+            {
+                await Task.Delay(500);
+                //DevTesting(_pm.GetPlayers());
+                var players = new Players();
+                players.Players_.AddRange(_pm.GetPlayers());
+                await responseStream.WriteAsync(players);
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Error, ex, "Client disconnected");
+                break;
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, "Something went wrong in updating the players");
+            }
         }
+    }
+
+
+
+
+    private void DevTesting(IEnumerable<Player> currentPlayers)
+    {
+        if (_pm.Count > 5000)
+        {
+            _pm.RemovePlayer(currentPlayers.ElementAt(Random.Shared.Next(0, _pm.Count - 1)).Name);
+        }
+        //for (int i = 0; i < currentPlayers.Length - 1; i++)
+        foreach (var player in currentPlayers)
+        //foreach (var player in currentPlayers.Where(x => Random.Shared.NextDouble() > .6))
+        {
+            if (Random.Shared.NextDouble() > .6)
+                continue;
+            player.Position.X += (Random.Shared.NextDouble() - .5) * 2;
+            player.Position.Y += (Random.Shared.NextDouble() - .5) * 2;
+
+            if (player.Position.X < -200 || player.Position.Y < -200 || player.Position.X > 200 || player.Position.Y > 200)
+            {
+                player.Position.X = 0;
+                player.Position.Y = 0;
+            }
+        }
+        _pm.AddPlayer(new Player()
+        {
+            Name = Random.Shared.Next().ToString(),
+            Position = new Position2D()
+            {
+                X = Random.Shared.NextDouble() * 200 - 100,
+                Y = Random.Shared.NextDouble() * 200 - 100,
+            }
+        });
     }
 }
