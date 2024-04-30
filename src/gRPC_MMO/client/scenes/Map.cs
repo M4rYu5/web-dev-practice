aef1b86613aa78b07d6a3f22730757e60781d331
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2024-04-30 19:34:11 +0300

    removed non-domain-specific and incomplete code from Map

--- src/gRPC_MMO/client/scenes/Map.cs
+++ src/gRPC_MMO/client/scenes/Map.cs
@@ -4,36 +4,15 @@ using Grpc.Net.Client;
 using ProximitySync;
 using System.Threading;
 using Grpc.Core;
+using MMOgRPC.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.Frozen;
+
+namespace Game;
 
 public partial class Map : Node3D
 {
 
 
-
-	// Called when the node enters the scene tree for the first time.
-	public override async void _Ready()
-	{
-		// The port number must match the port of the gRPC server.
-		using var channel = GrpcChannel.ForAddress("https://localhost:7197");
-		var client = new ProximityUpdater.ProximityUpdaterClient(channel);
-
-		//var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
-		//await foreach (var response in reply.ResponseStream.ReadAllAsync())
-		//{
-		//	GD.Print("Greeting: " + response.Message);
-		//}
-
-		var positionUpdater = client.UpdatePlayers(new Google.Protobuf.WellKnownTypes.Empty());
-		await foreach (var response  in positionUpdater.ResponseStream.ReadAllAsync())
-		{
-			GD.Print(System.Text.Json.JsonSerializer.Serialize(response.Players_));
-		}
-
-
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 }
