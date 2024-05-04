d77f355684f70461b98e74f1bb3c43c1aab7df5a
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2024-05-04 16:41:47 +0300

    server is now removing inactive playes

--- src/gRPC_MMO/server/ProximitySync/Data/PlayerManager.cs
+++ src/gRPC_MMO/server/ProximitySync/Data/PlayerManager.cs
@@ -7,12 +7,16 @@ namespace ProximitySync.Data
         public static readonly PlayerManager Instance = new();
 
         private readonly ConcurrentDictionary<string, Player> _players = [];
+        private readonly ConcurrentDictionary<string, DateTime> _playersLastUpdate = [];
 
 
-        private PlayerManager() { }
+        private PlayerManager()
+        {
+            MonitorAndRemoveInactivePlayers(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(10));
+        }
 
 
-        public int Count {  get => _players.Count; }
+        public int Count { get => _players.Count; }
 
 
         public ICollection<Player> GetPlayers()
@@ -26,11 +30,13 @@ namespace ProximitySync.Data
         public void AddPlayer(Player player)
         {
             _players.AddOrUpdate(player.Name, (string key) => player, (string key, Player p) => player);
+            _playersLastUpdate[player.Name] = DateTime.Now;
         }
 
         public void RemovePlayer(string playerName)
         {
             _players.TryRemove(playerName, out _);
+            _playersLastUpdate.TryRemove(playerName, out _);
         }
 
         public bool Contains(Player player) => Contains(player.Name);
@@ -46,6 +52,7 @@ namespace ProximitySync.Data
         public void Update(Player request)
         {
             _players.AddOrUpdate(request.Name, request, (key, oldValue) => request);
+            _playersLastUpdate[request.Name] = DateTime.Now;
         }
 
         public void Clear()
@@ -53,9 +60,29 @@ namespace ProximitySync.Data
             _players.Clear();
         }
 
-        internal Player[] ToArray()
+        /// <summary>
+        /// Monitors and removes inactive players from the game.
+        /// </summary>
+        /// <param name="inactivityCheckInterval">The interval at which player activity is checked</param>
+        /// <param name="inactivityThreshold">The duration of inactivity after which a player is considered inactive and removed</param>
+        private void MonitorAndRemoveInactivePlayers(TimeSpan inactivityCheckInterval, TimeSpan inactivityThreshold)
         {
-            return _players.Values.ToArray();
+            Task.Run(async () =>
+            {
+                DateTime currentTime;
+                while (true)
+                {
+                    currentTime = DateTime.Now;
+                    await Task.Delay(inactivityCheckInterval);
+                    foreach (var playerUpdate in _playersLastUpdate)
+                    {
+                        if (currentTime - playerUpdate.Value > inactivityThreshold)
+                        {
+                            RemovePlayer(playerUpdate.Key);
+                        }
+                    }
+                }
+            });
         }
     }
 }
