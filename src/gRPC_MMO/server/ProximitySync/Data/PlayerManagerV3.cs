075b1df5530e19dd88940deb96e6d37856adc0a1
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2024-05-15 05:49:43 +0300

    Tested with an empty PlayerManager
    With this test, I tried to completely remove the concurrency aspect of the app. The conclusion is that concurrency was not the problem, but rather the amount of data that had to be transmitted.
    
    To update 1000 players with information about all 1000 players, I would have to transmit at least 24MB of data per update frame:
    - 8 bytes: double for X position
    - 8 bytes: double for Y position
    - at least 4 bytes: string for Name (if it's empty, which certainly won't be).
    - and all of that 1000 * 1000 times (connections * num of updates) per 'frame'.

---
+++ src/gRPC_MMO/server/ProximitySync/Data/PlayerManagerV3.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Concurrent;
+
+namespace ProximitySync.Data
+{
+
+
+    /// <summary>
+    /// Note: This class is used to test if by removing it's functionality and returning a static list every time.
+    /// </summary>
+    public class PlayerManagerV3
+    {
+        public static readonly PlayerManagerV3 Instance = new();
+
+        private readonly List<Player> _players = [];
+
+
+        //private readonly ConcurrentDictionary<string, DateTime> _playersLastUpdate = [];
+
+
+        private PlayerManagerV3()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _players.Add(new Player());
+            }
+            //MonitorAndRemoveInactivePlayers(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(10));
+        }
+
+
+        public int Count { get => _players.Count; }
+
+
+        public List<Player> GetPlayers()
+        {
+            return _players;
+        }
+
+        /// <summary>
+        /// Adds or updates the player
+        /// </summary>
+        public void AddPlayer(Player player)
+        {
+
+            //_players.AddOrUpdate(player.Name, (string key) => player, (string key, Player p) => player);
+            //_playersLastUpdate[player.Name] = DateTime.Now;
+        }
+
+        public void RemovePlayer(string playerName)
+        {
+
+            //_players.TryRemove(playerName, out _);
+            //_playersLastUpdate.TryRemove(playerName, out _);
+        }
+
+        public bool Contains(Player player) => Contains(player.Name);
+
+        public bool Contains(string playerName)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Updates or add the player
+        /// </summary>
+        public void Update(Player request)
+        {
+
+            //_players.AddOrUpdate(request.Name, request, (key, oldValue) => request);
+            //_playersLastUpdate[request.Name] = DateTime.Now;
+        }
+
+        public void Clear()
+        {
+            
+        }
+
+
+    }
+}
